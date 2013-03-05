331e7db35c01bd8f79af36d347caf7f26bc395ce
Aleksey <sh-a-n@yandex.ru>
2013-03-05 21:36:57 +0400

    Initial Commit

---
+++ stackoverflow/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  stackoverflow
+//
+//  Created by Admin on 05.03.13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ stackoverflow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stackoverflow
+//
+//  Created by Admin on 05.03.13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
